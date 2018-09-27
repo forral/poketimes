78a05d9da565c03f33fc7f06246a378032fb9835
Luis Filipe Forra <forra.luis@me.com>
2018-09-27 22:34:43 +0100

    finish the two videos on introduction of react router

---
+++ src/components/Contact.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Contact = () => {
+
+  return (
+    <div className="container">
+      <h4 className="center">Contact</h4>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio sint sunt sed asperiores provident deleniti ea nam molestias, consequuntur unde? Accusamus consequatur provident similique dolore libero dolorum odio eius labore!</p>
+    </div>
+  );
+
+}
+
+export default Contact;
---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home = () => {
+
+  return(
+    <div className="container">
+      <h4 className="center">Home</h4>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio sint sunt sed asperiores provident deleniti ea nam molestias, consequuntur unde? Accusamus consequatur provident similique dolore libero dolorum odio eius labore!</p>
+    </div>
+  );
+
+}
+
+export default Home;
